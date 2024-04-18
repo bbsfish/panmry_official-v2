13eb5736885f862b077b86ca897bc33d0c59e92c
YSK <98343699+bbsfish@users.noreply.github.com>
2024-04-18 20:12:00 +0900

    npx astro add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-
+import vercel from "@astrojs/vercel/serverless";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
 // https://astro.build/config
 export default defineConfig({
-    // vite: {
-    //     resolve: {
-    //         alias: {
-    //             '@/*': [`${path.resolve(__dirname, 'src')}/*`]
-    //         }
-    //     }
-    // }
-});
+  output: "server",
+  adapter: vercel()
+} // vite: {
+//     resolve: {
+//         alias: {
+//             '@/*': [`${path.resolve(__dirname, 'src')}/*`]
+//         }
+//     }
+// }
+);
